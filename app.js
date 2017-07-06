3bd3583cab5c7c162c896c3a84fb2abdafcbb527
Tom Smith <wisdev53548@gmail.com>
2017-07-05 19:45:51 -0500

    Added a little test functionality to the request

--- app.js
+++ app.js
@@ -4,5 +4,8 @@ request({
 	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
 	json: true
 }, (error, response, body) => {
-	console.log(JSON.stringify(body, undefined, 2));
+	console.log(`Address: ${body.results[0].formatted_address}`);
+	console.log(`Your lattitude is ${body.results[0].geometry.location.lat}`);
+	console.log(`Your longitude is ${body.results[0].geometry.location.lng}`);
+	console.log(`Here, look for yourself: ${JSON.stringify(body.results[0].formatted_address)}`);
 });
